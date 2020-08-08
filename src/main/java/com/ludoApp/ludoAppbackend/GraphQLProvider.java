06a36785b880fdcd04d3c7c77f49a4a0c3c894be
psico <desenvolvedorjg@gmail.com>
2020-08-08 16:24:15 -0300

    [UPT] Begin to implements provider.

---
+++ src/main/java/com/ludoApp/ludoAppbackend/GraphQLProvider.java
@@ -0,0 +1,35 @@
+package com.ludoApp.ludoAppbackend;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.URL;
+
+@Component
+public class GraphQLProvider {
+
+    private GraphQL graphQL;
+
+    @Bean
+    public GraphQL graphQL() {
+        return graphQL;
+    }
+
+    @PostConstruct
+    public void init() throws IOException {
+        URL url = Resources.getResource("schema.graphqls");
+        String sdl = Resources.toString(url, Charsets.UTF_8);
+        GraphQLSchema graphQLSchema = buildSchema(sdl);
+        this.graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+    }
+
+    private GraphQLSchema buildSchema(String sdl) {
+        // TODO: we will create the schema here later
+    }
+}
