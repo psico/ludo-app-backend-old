d0fcf31fdeae1b96d8dbfcd1b4a4b264e1f4b414
C1314089 Joao Gabriel dos Santos Rodrigues <c1314089@interno.bb.com.br>
2020-08-08 15:21:23 -0300

    [ADD] Started project.

---
+++ src/main/java/com/ludoApp/ludoAppbackend/LudoAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ludoApp.ludoAppbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudoAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LudoAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ludoApp/ludoAppbackend/LudoAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ludoApp.ludoAppbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LudoAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
