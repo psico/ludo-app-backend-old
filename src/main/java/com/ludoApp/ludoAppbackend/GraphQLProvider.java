e7e522e840cc93542b23c6572b12075b6919d5d0
psico <desenvolvedorjg@gmail.com>
2020-08-22 16:42:02 -0300

    [ADD] First try to connect to firebase

--- src/main/java/com/ludoApp/ludoAppbackend/GraphQLProvider.java
+++ src/main/java/com/ludoApp/ludoAppbackend/GraphQLProvider.java
@@ -1,7 +1,10 @@
 package com.ludoApp.ludoAppbackend;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import graphql.GraphQL;
 import graphql.schema.GraphQLSchema;
 import graphql.schema.idl.*;
@@ -10,6 +13,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 
@@ -42,6 +47,31 @@ public class GraphQLProvider {
     }
 
     private RuntimeWiring buildWiring() {
+        FirebaseOptions options;
+        try (FileInputStream serviceAccount = new FileInputStream("ludoapp-b612-firebase-adminsdk-4c863-3aa26efa30.json")) {
+
+            options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://ludoapp-b612.firebaseio.com")
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+//        FirebaseOptions options = new FirebaseOptions.Builder()
+//                .setCredentials(GoogleCredentials.getApplicationDefault())
+//                .setDatabaseUrl("https://ludoapp-b612.firebaseio.com/")
+//                .build();
+//
+//        FirebaseApp.initializeApp(options);
+
+
         return RuntimeWiring.newRuntimeWiring()
                 .type(TypeRuntimeWiring.newTypeWiring("Query")
                         .dataFetcher("bookById", graphQLDataFetchers.getBookByIdDataFetcher())
