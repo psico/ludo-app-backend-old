dc27504f43a1272cff2b8ed6963f9056001dbff5
psico <desenvolvedorjg@gmail.com>
2020-08-29 17:13:27 -0300

    [UPT] Put the code in correct position

--- src/main/java/com/ludoApp/ludoAppbackend/GraphQLDataFetchers.java
+++ src/main/java/com/ludoApp/ludoAppbackend/GraphQLDataFetchers.java
@@ -77,42 +77,6 @@ public class GraphQLDataFetchers {
     );
 
     public DataFetcher getBookByIdDataFetcher() {
-        FirebaseOptions options;
-        try (FileInputStream serviceAccount = new FileInputStream("firebase_connection.json")) {
-
-            options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .setDatabaseUrl("https://ludoapp-b612.firebaseio.com")
-                    .build();
-
-            FirebaseApp.initializeApp(options);
-            Firestore db = FirestoreClient.getFirestore();
-
-
-            ApiFuture<QuerySnapshot> query = db.collection("usersInfo").get();
-
-            QuerySnapshot querySnapshot = query.get();
-            List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
-            for (QueryDocumentSnapshot document : documents) {
-                System.out.println(document);
-                System.out.println("UID: " + document.getId());
-                System.out.println("Name: " + document.getString("name"));
-//                if (document.contains("middle")) {
-//                    System.out.println("Middle: " + document.getString("middle"));
-//                }
-//                System.out.println("Last: " + document.getString("last"));
-//                System.out.println("Born: " + document.getLong("born"));
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-
         return dataFetchingEnvironment -> {
             String bookId = dataFetchingEnvironment.getArgument("id");
             return books
@@ -156,6 +120,43 @@ public class GraphQLDataFetchers {
     }
 
     public DataFetcher getFriendsDataFetcher() {
+        FirebaseOptions options;
+        try (FileInputStream serviceAccount = new FileInputStream("firebase_connection.json")) {
+
+            options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://ludoapp-b612.firebaseio.com")
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+            Firestore db = FirestoreClient.getFirestore();
+
+
+            ApiFuture<QuerySnapshot> query = db.collection("usersInfo").get();
+
+            QuerySnapshot querySnapshot = query.get();
+            List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
+            for (QueryDocumentSnapshot document : documents) {
+                System.out.println(document);
+                System.out.println("UID: " + document.getId());
+                System.out.println("Name: " + document.getString("name"));
+//                if (document.contains("middle")) {
+//                    System.out.println("Middle: " + document.getString("middle"));
+//                }
+//                System.out.println("Last: " + document.getString("last"));
+//                System.out.println("Born: " + document.getLong("born"));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+
         return dataFetchingEnvironment -> {
 //            Map<String,String> userInfo = dataFetchingEnvironment.getSource();
             return friendsList;
