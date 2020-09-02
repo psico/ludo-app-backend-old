cfd3975afaed8ba0b8f50a95836bb43e023939a8
psico <desenvolvedorjg@gmail.com>
2020-09-02 11:43:26 -0300

    [UPT] Using friend model

--- src/main/java/com/ludoApp/ludoAppbackend/UserInfo.java
+++ src/main/java/com/ludoApp/ludoAppbackend/UserInfo.java
@@ -1,15 +1,20 @@
 package com.ludoApp.ludoAppbackend;
 
+import java.util.List;
+
 public class UserInfo {
 
     public String uid;
     public String name;
+    public List<Friend> friends;
 
-    public UserInfo(String uid, String name) {
+    public UserInfo(String uid, String name, List<Friend> friends) {
         this.uid = uid;
         this.name = name;
+        this.friends = friends;
 
         System.out.println(uid);
         System.out.println(name);
+        System.out.println(friends);
     }
 }
