ac4435f8938c91d1c27d414e644fe87342b8875d
psico <desenvolvedorjg@gmail.com>
2020-09-02 11:42:52 -0300

    [ADD] Added friend model

---
+++ src/main/java/com/ludoApp/ludoAppbackend/Friend.java
@@ -0,0 +1,15 @@
+package com.ludoApp.ludoAppbackend;
+
+public class Friend {
+
+    public String uid;
+    public String name;
+
+    public Friend(String uid, String name) {
+        this.uid = uid;
+        this.name = name;
+
+        System.out.println(uid);
+        System.out.println(name);
+    }
+}
