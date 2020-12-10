0b157c4393e03d9ece660b628f9758015895b47a
psico <desenvolvedorjg@gmail.com>
2020-12-09 21:33:30 -0300

    [UPT] Another try to fix fetcher

--- src/main/java/com/ludoApp/ludoAppbackend/GraphQLDataFetchers.java
+++ src/main/java/com/ludoApp/ludoAppbackend/GraphQLDataFetchers.java
@@ -99,7 +99,7 @@ public class GraphQLDataFetchers {
         List<Map<String, Object>> result = new ArrayList<>();
 
         try {
-            QuerySnapshot querySnapshot = this.db.collection("matches").get().get();
+            QuerySnapshot querySnapshot = FirestoreClient.getFirestore().collection("matches").get().get();
             List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
 
             for (QueryDocumentSnapshot document : documents) {
